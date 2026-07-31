04d136ffc5a36152a7ec06db427785cfaee1194f
nxzeh727 <eh727nxzrin@gmail.com>
2026-07-31 17:35:06 +0000

    restructure: move frontend out of ploot_stuff to repo root

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 import Calendar from './calendar'
 import Todo from './todo'
