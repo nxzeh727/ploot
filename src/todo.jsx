80d01eb5a208cfcc8c0d8cd26f5acb3a66323640
nxzeh727 <eh727nxzrin@gmail.com>
2026-08-05 04:58:04 +0000

    made final calendar editable and allowed it to be saved into ics format(can be opened in google calendar :D)

--- src/todo.jsx
+++ src/todo.jsx
@@ -5,7 +5,8 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import { parseDragMeta, sortEventSegs } from '@fullcalendar/core/internal'
 import { useEditor, EditorContent,useEditorState } from '@tiptap/react'
-import { TaskItem, TaskList } from '@tiptap/extension-list'
+import TaskList from '@tiptap/extension-task-list'
+import TaskItem from '@tiptap/extension-task-item'
 import StarterKit from '@tiptap/starter-kit'
 import { supabase } from './supabaseClient'
 
@@ -16,9 +17,40 @@ function Todo() {
     const [loading,setLoading] = useState(false)
   
     const editor = useEditor({
-            extensions: [StarterKit, TaskItem, TaskList, TaskItem.configure({nested:true,})],
+            extensions: [StarterKit, TaskList, TaskItem.configure({nested:true,})],
             content: '<p>what do you need to do today?</p>'
         })
+    const acceptCalendar = () => {
+        const lines = [
+            'BEGIN:VCALENDAR',
+            'VERSION:2.0',
+            'PRODID:-//Ploot//EN'
+
+        ]
+        schedule.forEach(event => {
+            const start = new Date(event.start).toISOString().replace(/[-:]/g,'').split('.')[0]+'Z'
+            const end = new Date(event.end).toISOString().replace(/[-:]/g,'').split('.')[0]+'Z'
+            const title = event.title
+            const description = event.extendedProps.description
+            lines.push(
+            `BEGIN:VEVENT`,
+            `DTSTART:${start}`,
+            `DTEND:${end}`,
+            `SUMMARY:${title}`,
+            `DECRIPTION:${description}`,
+            `END:VEVENT`,
+        )
+        })
+        lines.push(`END:VCALENDAR`)
+        const blob = new Blob([lines.join(`\r\n`)], { type: 'text/calendar'})
+        const url = URL.createObjectURL(blob)
+        const cheese = document.createElement('a')
+        cheese.href = url
+        cheese.download = 'ploots_sprouts.ics'
+        cheese.click()
+        URL.revokeObjectURL(url)
+
+    }
     
     const saveNotes = async () => {
         if (!editor) return
@@ -58,8 +90,8 @@ function Todo() {
   return (
     <>
         {page === 'todo' && 
-            (<><div class="cheese">
-                <div class="toolbar">
+            (<><div className="cheese">
+                <div className="toolbar">
                     <button onClick={() => editor.chain().focus().toggleTaskList().run()}
                     >
                         task list :)
@@ -73,7 +105,7 @@ function Todo() {
                         bullet points :)
                     </button>
                 </div>
-                <EditorContent editor={editor} class="potato"/>
+                <EditorContent editor={editor} className="potato"/>
                 <button onClick={saveNotes} disabled={loading}>
                 {loading ? "generating schedule" : "save"}
                 </button>
@@ -86,10 +118,29 @@ function Todo() {
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
             initialView="timeGridDay"
             events={schedule}
+            editable={true}
+            eventResizableFromStart={true} 
+            eventDrop={(info)=>{
+                setSchedule(prev => prev.map(e => 
+                    e.title === info.event.title
+                    ?  { ... e, start: info.event.startStr, end: info.event.endStr}
+                    : e
+                ))
+            }}
+            
+            eventResize={(info)=>{
+                setSchedule(prev => prev.map(e => 
+                    e.title === info.event.title
+                    ?  { ... e, start: info.event.startStr, end: info.event.endStr}
+                    : e
+                ))
+            }}
             eventClick={(info)=>{
                 alert(`${info.event.title}\n\n${info.event.extendedProps.description}`)
             }}
             />
+            <button onClick={acceptCalendar} disabled={loading}> {loading ? "saving..." : "accept calendar"}</button>
+            
 
         </div></>)}
     </>
